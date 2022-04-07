b8ae7195361b7032aefc8e7cdc1e5aa5f563f224
Carlos Cruz <cruzprww2@gmail.com>
2022-04-07 10:40:41 -0500

    Inline and External JS Exercise

--- js/external.js
+++ js/external.js
@@ -1,4 +1,17 @@
 "use strict";
+alert("Welcome to my Website!")
+let userColorAnswer = prompt("What is your favorite color?");
+    console.log("Great!" + " " + userColorAnswer + " " + "is my favorite color too!");
+
+/*
 
 
-console.log("Hello, from external JavaScript");
+
+
+
+
+
+
+
+*/
+console.log("Hello from external JavaScript!");
