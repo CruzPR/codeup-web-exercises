21680ac877a596f6a3d505598d5984c930e181c3
Carlos Cruz <cruzprww2@gmail.com>
2022-04-12 16:01:59 -0500

    Added loops.html and for_loops.js, also modified(comments) loops-mini-exercises.html

---
+++ js/for_loops.js
@@ -0,0 +1,56 @@
+"use  strict";
+
+(function(){
+ var a = 0;
+   function showMultiplicationTable(num){
+       for(var i = 1 ; i < 11 ;i++ ){
+           console.log(num * i);
+       }
+   }
+showMultiplicationTable(7);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})();
