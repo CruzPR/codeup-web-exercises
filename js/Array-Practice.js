a4bc60dc5345c19e3cb31849a809c2a5c404bbb1
Carlos Cruz <cruzprww2@gmail.com>
2022-04-14 08:53:17 -0500

    Added Array-Practice.html and Array-Practice.js

---
+++ js/Array-Practice.js
@@ -0,0 +1,74 @@
+"use strict";
+
+(function (){
+
+    // !! Write a function, returnLongString, that takes in an array of strings and returns all strings concatenated together !!
+
+
+
+
+
+
+
+
+
+    // !! Write a function, returnArrSum, that takes in an array of values and returns the sum of all number elements !!
+
+
+
+
+
+
+
+
+
+
+    /* !! Write a function, checkIfNumInArray, that takes in an array of numbers and a number and returns the string
+            'input num in input array' if the second argument is an element in the first argument array
+            otherwise, the function should return the string 'input num NOT in input array'
+       !!
+     */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})();
