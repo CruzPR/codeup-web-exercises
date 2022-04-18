4b2d2b6fbe92818829e11b3e9c15f64dc91c4eff
Carlos Cruz <cruzprww2@gmail.com>
2022-04-17 21:49:26 -0500

    Worked on Function Drills 04/17/2022 at 10pm.

--- js/Function-Drills.js
+++ js/Function-Drills.js
@@ -795,16 +795,27 @@
     
 // Even More Function Bonuses
 // Create a function that will return how many whitespace characters are at the beginning and end of a string.
-//
+    function whiteSpaceCount(str) {
+        var spaceCount = str.length - str.trimLeft().length + str.length - str.trimRight().length;
+        return spaceCount;
+    }
+    console.log(whiteSpaceCount("  Hi, how many whitespaces are in this string?  ")) //4
+    
 // Create a function that takes in two string inputs.
 //
 // If the second string input is present in the first, return the first input string with the second input string removed from it.
 // If the second string input is present multiple times in the first, the second string will only be removed where it first occurs in the first string.
 // If the second string input is not present in the first, return the first string as entered in the function.
+    
+    
+    
 // Create a function that takes in a string and returns true if the last letter is an "a" (otherwise, return false).
-//
 // EXTRA CHALLENGE: create a function that will return how many whitespace characters are at the beginning of a string (hint: use regex).
-//
+    
+    
+    
+    
+
 // Create a function returnTrueMessage() that returns the string "Hey, it's true!"
 //
 // Create a function returnFalseMessage() that returns the string "Hey, it's false!"
