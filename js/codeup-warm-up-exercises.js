40454cd215e1197c4cfc1de7a0ccd8018feba74b
Carlos Cruz <cruzprww2@gmail.com>
2022-04-27 09:38:18 -0500

    Worked on js/codeup-warm-up-exercises.js on 04/27/2022

--- js/codeup-warm-up-exercises.js
+++ js/codeup-warm-up-exercises.js
@@ -66,9 +66,23 @@
       }
     */
 
+    // TERNARY OPERATOR
+    function getFee(isMember) {
+        return (isMember ? '$2.00' : '$10.00'); // (condition ? 'truthy' : 'falsy';
+    }
 
+    console.log(getFee(true));
+// expected output: "$2.00"
 
+    console.log(getFee(false));
+// expected output: "$10.00"
 
+    console.log(getFee(null));
+// expected output: "$10.00"
+
+    var age = 26;
+    var beverage = (age >= 21) ? "Beer" : "Juice";
+    console.log(beverage); // "Beer"
 
 
 
