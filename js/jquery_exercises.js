1d1980fd3c963561eac8ab9ec34e87bc2e70d2f4
Carlos Cruz <cruzprww2@gmail.com>
2022-05-13 11:28:33 -0500

    Followed along the lecture

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,8 +1,23 @@
 "use strict";
 $(document).ready(function(){
-   alert("ready");
-
-
+    // A lot cleaner with JQuery method!
+    // ID Selector
+    $("#pageLink").html("<h1>Link to somewhere</h1>");
+    // Less clean with the HTML method!
+        document.getElementById("pageLink").innerHTML = "<h1>Link to somewhere</h1>";
+        
+    // Class selector
+    // $(".list-item")[0].innerHTML = "Hello";
+    // Element Selector
+        $("*").css({   //Object //Multiple elements
+            "background-color": "maroon",
+            "color": "red",
+            "font-size": "25px",
+            "font-weight": "bolder",
+            "border": "23px double forestgreen",
+            "border-radius": "10px",
+            })
+            
 
 
 
