d97ad704462d175413897dcd1a84a0a813343807
Carlos Cruz <cruzprww2@gmail.com>
2022-05-20 15:09:48 -0500

    Continue to work on  js/weather-map.js and weather_map.html

--- js/weather-map.js
+++ js/weather-map.js
@@ -14,6 +14,7 @@ $.get("http://api.openweathermap.org/data/2.5/onecall", {
     lon:   -98.48527,
     units: "imperial"
 }).done(function(data) {
+    const months = ["JAN", "FEB", "MAR","APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
     console.log('The entire response:', data);
     console.log('Diving in - here is current information: ', data.current);
     console.log('A step further - information for tomorrow: ', data.daily[1]);
@@ -21,18 +22,24 @@ $.get("http://api.openweathermap.org/data/2.5/onecall", {
     console.log(data.current.weather[0].description);
     // loop through daily
     console.log(data.daily); // it is an array, each day is an object
+    $("current").append("<p>" + data.current.temp + "</p>")
+    
     data.daily.forEach(function(dailyForecast, index){
         if(index < 5){
-        
-            console.log(dailyForecast.temp.max); // console.log each step of the way. 
-            console.log(dailyForecast.temp.min);
+            let humidity = "Humidity:"
+            let datetime = new Date(dailyForecast.dt * 1000);
+            console.log(datetime)
+            // console.log(datetime.getMonth() + " " + datetime.getDate());
+            let dateMonth = months[datetime.getMonth()] + " " + datetime.getDate();
+            $("#forecast").append("<h3>" + datetime + "</h3>" + "<p>" + dailyForecast.temp.min + "</p>" + "<p>" + dailyForecast.temp.max + "</p>" + "<p>" + humidity + " " + dailyForecast.humidity +"%"+ "</p>");
+            
             
         }
         
         
         
         
-    })
+    });
     
     
     
