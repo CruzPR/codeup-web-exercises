eb81192016fdc548b95710b1fef781744194cf8f
Carlos Cruz <cruzprww2@gmail.com>
2022-05-24 16:00:33 -0500

    Finished ES6-lecture.html and ES6-lecture.js

--- js/ES6-lecture.js
+++ js/ES6-lecture.js
@@ -10,5 +10,21 @@ let dessertMessage = `Well, I love ${greatDessert} but I don't care for ${badDes
 console.log(dessertMessage);
 
 
+let message = "";
+const names = ['John', 'Paul', 'George', 'Ringo'];
+// for (var i = 0; i < names.length; i += 1) {
+//     message += 'Hello, ' + names[i] + '\n';
+// }
+
+for(let name of names) {
+    message += `Hello, ${name} \n`;
+}
+
+console.log(message);
 
 
+// ============== MINI EXERCISE
+
+// TODO: Refactor the following code to use ES6 syntax. The output should stay the same.
+
+     const doubleInput = input => input * 2;
