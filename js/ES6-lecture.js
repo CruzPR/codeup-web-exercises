49943863ca7de1685cdea671d25930a01fed5edb
Carlos Cruz <cruzprww2@gmail.com>
2022-05-24 15:17:27 -0500

    Added ES6-lecture.html and ES6-lecture.js

---
+++ js/ES6-lecture.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// ============== MINI EXERCISE
+
+// TODO: Refactor the following code to use ES6 syntax. The output should stay the same.
+
+const greatDessert = 'banana pudding';
+const badDessert = 'rhubarb pie';
+let dessertMessage = `Well, I love ${greatDessert} but I don't care for ${badDessert}.`;
+console.log(dessertMessage);
+
+
+
+
