7c85ef55bfd847f75587d7742b877d51d8b84312
Carlos Cruz <cruzprww2@gmail.com>
2022-05-25 09:16:15 -0500

    Created and finished arrow.function.warmup.html and arrow-function-warmup.js

---
+++ js/arrow-function-warmup.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// TODO: Write an arrow function named "highFive" which accepts a string and alerts the user's input, asking for a high five.
+// Example: highFive("Go") returns "Go, high five!"
+
+   const highFive = string => alert(`${string}, high five!`);
+   highFive("Go");
